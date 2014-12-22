486fc980b41259e86dddeff039a263bb7c097d51
Guillaume Friloux <guillaume@friloux.me>
2014-12-22 10:40:47 +0100

    Add strptime(). Simplify implementation of localtime_r.
    Patches from vtorri.

--- src/lib/evil_time.h
+++ src/lib/evil_time.h
@@ -6,6 +6,7 @@
  * @file evil_time.h
  * @brief The file that provides functions ported from Unix in time.h.
  * @defgroup Evil_Time_Group Time.h functions
+ * @ingroup Evil
  *
  * This header provides functions ported from Unix in time.h.
  *
@@ -37,20 +38,24 @@ EAPI struct tm *localtime_r(const time_t *timep, struct tm *result);
 
 #endif /* localtime_r */
 
-#ifdef UNDER_CE
-
 /**
- * @brief Stub implementation of tzset().
+ * @brief Convert a string representation of time to a time tm structure .
  *
- * This function does nothing.
+ * @param buf The string to convert.
+ * @param fmt The representation of time.
+ * @aram tm The time tm structure.
+ * @return The first character not processed in this function call.
+ *
+ * This function converts the string @p s to a time tm structure and
+ * fill the buffer @p tm. The format of the time is specified by
+ * @p format. on success, this function returns the first character
+ * not processed in this function call, @c NULL otherwise.
  *
  * Conformity: Non applicable.
  *
- * Supported OS: Windows CE.
+ * Supported OS: Windows XP.
  */
-EAPI void tzset(void);
-
-#endif
+EAPI char *strptime(const char *buf, const char *fmt, struct tm *tm);
 
 
 /**
